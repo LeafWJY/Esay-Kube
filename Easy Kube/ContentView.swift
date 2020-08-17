fc285127599a2326cf54f0e3c0dac96d7771965c
王镓耀 <jiayao.wang@hand-china.com>
2020-08-17 16:41:18 +0800

    Initial Commit

---
+++ Easy Kube/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Easy Kube
+//
+//  Created by 王镓耀 on 2020/8/17.
+//  Copyright © 2020 codewjy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
